5fcb9ec2299037b4ab83b80570a9a34de40f8a8f
xuanmos <me@xuanmo.xin>
2018-12-19 15:49:07 +0800

    udate

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
   lintOnSave: true,
-
+  baseUrl: process.env.VUE_APP_BASE_URL,
+  outputDir: process.env.VUE_APP_OUTPUTDIR,
   devServer: {
     overlay: {
       warnings: true,
@@ -16,7 +19,6 @@ module.exports = {
       }
     }
   },
-
   css: {
     loaderOptions: {
       sass: {
@@ -30,9 +32,20 @@ module.exports = {
       }
     }
   },
-
   productionSourceMap: false,
-
+  configureWebpack: config => {
+    return {
+      plugins: [
+        new CopyWebpackPlugin([
+          {
+            from: path.resolve(__dirname, './static'),
+            to: 'static',
+            ignore: ['.*']
+          }
+        ])
+      ]
+    }
+  },
   pluginOptions: {
     'cube-ui': {
       postCompile: true,
