e42a26940e56a9c416977ad5df44d81061cc3e94
xuanmos <me@xuanmo.xin>
2018-12-25 12:26:16 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,3 +9,15 @@ export const showToast = config => store.dispatch('showToast', config)
 export const hideToast = () => store.dispatch('hideToast')
 
 export const showConfirm = config => store.dispatch('showConfirm', config)
+
+// 获取查询参数
+export const getQueryParams = params => {
+  const obj = {}
+  const _params = window.decodeURIComponent(params)
+  _params.split('&').map(item => {
+    item.replace(/([^?&]*)=([^?&]*)/, (match, $1, $2) => {
+      obj[$1] = $2
+    })
+  })
+  return obj
+}
