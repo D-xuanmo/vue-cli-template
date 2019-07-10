4bc92ea2acb5007b758c9176754f402dbf163d4a
xuanmos <me@xuanmo.xin>
2019-07-10 10:11:08 +0800

    update

--- src/pages/test/main.vue
+++ src/pages/test/main.vue
@@ -1,26 +1,16 @@
 <template>
   <div class="container">
-    <!-- <div class="content" v-html="result"></div> -->
-    <x-table :tableData="tableData"></x-table>
+    <div class="content" v-html="result"></div>
     我是svg-icon组件： <x-svg-icon iconName="qq"></x-svg-icon>
   </div>
 </template>
 <script>
 import API from '@/api'
-import XTable from '@/components/Table'
 export default {
   name: 'Test',
-  components: {
-    XTable
-  },
   data () {
     return {
-      result: '',
-      tableData: {
-        config: {
-          cssClass: ['xx']
-        }
-      }
+      result: ''
     }
   },
   created () {
